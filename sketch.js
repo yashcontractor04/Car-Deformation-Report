5ef545f22d8067b0ddcc4f50578c7a8dfbb9a8a9
yashcontractor04 <67055888+yashcontractor04@users.noreply.github.com>
2020-06-29 17:53:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,22 +8,24 @@ function setup() {
   //createSprite(400, 200, 50, 50);
   car = createSprite(50, 200, 50, 50);
   car.velocityX = speed;
-  wall = createSprite(1500, 200, 60, height/2);
-  wall.shapeColor = (80,80,80);
+  wall = createSprite(1500, 200, 20, height/2);
+  wall.shapeColor = color(255,0,0);
+  car.debug = true;
+  wall.debug = true;
 }
 
 function draw() {
   background(0);  
-  if(wall.x - car.x === wall.width/2 + car.width/2) {
+  if(wall.x - car.x <  wall.width/2 + car.width/2) {
     car.velocityX = 0;
     if((0.5*weight*speed*speed)/22500 <= 100) {
-      car.shapeColor = (0,255,0);
+      car.shapeColor = color(0,255,0);
     }
     if((0.5*weight*speed*speed)/22500 > 100 && (0.5*weight*speed*speed)/22500 < 180) {
-      car.shapeColor = (230, 230, 0);
+      car.shapeColor = color(230, 230, 0);
     }
     if((0.5*weight*speed*speed)/22500 >= 180) {
-      car.shapeColor = (255,0,0);
+      car.shapeColor = color(255,0,0);
     }
   }
   drawSprites();
